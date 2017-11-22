871d38989aff190c2973cb9ce803efe42b7d6fa9
ethanzh <ethan.houston@gmail.com>
2017-11-22 09:32:31 -0800

    Working on prereqs

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def map_and_filter(s, map_fn, filter_fn):
     """
     # BEGIN Question 0
     "*** REPLACE THIS LINE ***"
-    return ['REPLACE THIS WITH YOUR LIST COMPREHENSION']
+    return [map_fn(i) for i in s if filter_fn(i)]
     # END Question 0
 
 def key_of_min_value(d):
@@ -31,7 +31,7 @@ def key_of_min_value(d):
     """
     # BEGIN Question 0
     "*** REPLACE THIS LINE ***"
-    return min('REPLACE THIS WITH YOUR SOLUTION')
+    return min(d, key=d.get)
     # END Question 0
 
 def zip(*sequences):
